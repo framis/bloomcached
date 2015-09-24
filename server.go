a9e147928b50cd27d8a92713c17b2e3e4f1ba160
Francois Misslin <francois@revinate.com>
2015-09-23 21:55:47 -0700

    Refactored server.go for easier testing

--- server.go
+++ server.go
@@ -2,43 +2,54 @@ package main
 
 import (
 	"bufio"
-
 	"bytes"
 	"fmt"
 	"github.com/willf/bloom"
 	"net"
-	"os"
 	"strconv"
 )
 
 const (
-	CONN_HOST           = "localhost"
-	CONN_PORT           = 3333
-	CONN_TYPE           = "tcp"
-	NUMBER_OF_ELEMENTS  = 1000000
-	MULT                = 20
-	NUMBER_OF_HASH_FUNC = 5
+	CONN_HOST = "localhost"
+	CONN_PORT = 3333
+	SIZE      = 1000000 // Size of the Bloom filter
+	HASH      = 5       // Number of Hash functions
 )
 
 type (
-	Server struct {
+	ClientConn struct {
 		conn net.Conn
 		f    *bloom.BloomFilter
+	}
+
+	Server struct {
+		listener net.Listener
+		f        *bloom.BloomFilter
 	}
 )
 
 func main() {
-	server, err := net.Listen(CONN_TYPE, CONN_HOST+":"+strconv.Itoa(CONN_PORT))
+	server, err := NewServer(CONN_HOST+":"+strconv.Itoa(CONN_PORT), SIZE, HASH)
 	if server == nil {
 		panic("couldn't start listening: " + err.Error())
 	}
-	f := bloom.New(MULT*NUMBER_OF_ELEMENTS, NUMBER_OF_HASH_FUNC)
-	conns := clientConns(server)
+	conns := clientConns(server.listener)
 	for {
-		go handleConn(&Server{conn: <-conns, f: f})
+		go handleConn(&ClientConn{conn: <-conns, f: server.f})
 	}
 }
 
+// Creates a new TCP Server with a bloom filter (m, k)
+func NewServer(dsn string, m uint, k uint) (*Server, error) {
+	listener, err := net.Listen("tcp", dsn)
+	if listener == nil {
+		return nil, err
+	}
+	f := bloom.New(m, k)
+	return &Server{listener: listener, f: f}, nil
+}
+
+// Accepts connections
 func clientConns(listener net.Listener) chan net.Conn {
 	ch := make(chan net.Conn)
 	i := 0
@@ -58,19 +69,21 @@ func clientConns(listener net.Listener) chan net.Conn {
 	return ch
 }
 
-func handleConn(s *Server) {
-	reader := bufio.NewReader(s.conn)
+// Handles connections: from a request, send a response
+func handleConn(c *ClientConn) {
+	reader := bufio.NewReader(c.conn)
 	for {
 		request, err := reader.ReadBytes('\n')
 		if err != nil { // EOF, or worse
 			break
 		}
-		s.prepareResponse(request)
+		c.writeResponse(request)
 	}
-	s.conn.Close()
+	c.conn.Close()
 }
 
-func (s *Server) prepareResponse(request []byte) {
+// Write the response from the request
+func (c *ClientConn) writeResponse(request []byte) {
 	message := bytes.Split(request, []byte{'|'})
 	var response []byte
 	if len(message) != 2 {
@@ -81,25 +94,17 @@ func (s *Server) prepareResponse(request []byte) {
 	item := bytes.TrimSuffix(message[1], []byte("\n"))
 
 	if verb == "TEST" {
-		if s.f.Test(item) {
+		if c.f.Test(item) {
 			response = []byte("200|true\n")
 		} else {
 			response = []byte("200|false\n")
 		}
 	} else if verb == "ADD" {
-		s.f.Add(item)
+		c.f.Add(item)
 		response = []byte("201\n")
 	} else {
 		response = []byte("400|Unknown Verb\n")
 	}
-	s.conn.Write(response)
+	c.conn.Write(response)
 	fmt.Println("Request:", string(request), "Response: ", string(response))
-}
-
-func checkError(err error) {
-	if err != nil {
-		fmt.Println("hi")
-		fmt.Fprintf(os.Stderr, "Error: %s", err.Error())
-		os.Exit(1)
-	}
 }

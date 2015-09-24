ae83ef4bcc8a79486eac2ff2775cc67eb85a8e5c
Francois Misslin <francois@revinate.com>
2015-09-23 21:20:07 -0700

    Fixed client

--- client.go
+++ client.go
@@ -24,21 +24,21 @@ func NewClient(dsn string) (*Client, error) {
 // If response is 0, the item is definitely not in the filter
 func (c *Client) Test(item string) (bool, error) {
 	writer := bufio.NewWriter(c.conn)
-	_, err := writer.WriteString(item + "\n")
+	_, err := writer.WriteString("TEST|" + item + "\n")
 	if err != nil {
 		return false, err
 	}
 	writer.Flush()
-	buf := make([]byte, 1024)
+	buf := make([]byte, 2048)
 	n, err := c.conn.Read(buf)
-	return (buf[0:n] == "200"), err
+	return (string(buf[0:n]) == "200|true\n"), err
 }
 
 func (c *Client) Add(item string) (bool, error) {
 	writer := bufio.NewWriter(c.conn)
-	_, err := writer.WriteString(item + "\n")
+	_, err := writer.WriteString("ADD|" + item + "\n")
 	writer.Flush()
-	buf := make([]byte, 1024)
+	buf := make([]byte, 2048)
 	n, err := c.conn.Read(buf)
-	return (buf[0:n] == "201"), err
+	return (string(buf[0:n]) == "201\n"), err
 }
